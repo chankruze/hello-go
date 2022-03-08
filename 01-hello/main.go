a76063dd18316657b52f2fd265a59f8affe9dee4
chankruze <chankruze@gmail.com>
2022-03-08 17:42:23 +0530

    hello go!
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ 01-hello/main.go
@@ -0,0 +1,16 @@
+/*
+Author: chankruze (chankruze@gmail.com)
+Created: Tue Mar 08 2022 17:35:01 GMT+0530 (India Standard Time)
+
+Copyright (c) geekofia 2022 and beyond
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello Go!\n")
+	fmt.Printf("Hello Go!%d\n", 69)
+	fmt.Println("Hello Go!")
+}
